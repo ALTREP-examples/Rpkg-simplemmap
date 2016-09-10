adadafa5d8af806fe910895b78f2951bdab3a455
Luke Tierney <luke-tierney@uiowa.edu>
2016-09-10 11:28:27 -0500

    Added possible values for type, checked with match.arg.

--- R/simplemmap.R
+++ R/simplemmap.R
@@ -1,4 +1,5 @@
-mmap <- function(filename, type, ptrOK = FALSE, wrtOK = FALSE) {
+mmap <- function(filename, type = c("double", "integer", "int"),
+                 ptrOK = FALSE, wrtOK = FALSE) {
+    type = match.arg(type)
     .External(C_mmap_file, filename, type, ptrOK, wrtOK)
 }
-
