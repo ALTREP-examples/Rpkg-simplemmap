88fa6607792587d6a8cc7f246741aaf8f4b64d22
Luke Tierney <luke-tierney@uiowa.edu>
2016-10-05 16:55:19 -0500

    dded serOK argument for controlling serialization policy.

--- R/simplemmap.R
+++ R/simplemmap.R
@@ -2,9 +2,9 @@
 # Distributed under GPL 2 or 3
 
 mmap <- function(filename, type = c("double", "integer", "int"),
-                 ptrOK = TRUE, wrtOK = FALSE) {
+                 ptrOK = TRUE, wrtOK = FALSE, serOK = TRUE) {
     type = match.arg(type)
-    .External(C_mmap_file, filename, type, ptrOK, wrtOK)
+    .External(C_mmap_file, filename, type, ptrOK, wrtOK, serOK)
 }
 
 munmap <- function(x)
